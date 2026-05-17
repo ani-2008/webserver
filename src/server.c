b2a73f5c9ca0fcad94c21b84647d1f253b1f1d99
bitabyte <bitabyte7@gmail.com>
2026-05-17 19:00:22 +0530

    separate funcs for error codes 500, 404

--- src/server.c
+++ src/server.c
@@ -12,17 +12,50 @@
 #define RECV_SIZE 1024
 #define BUF_SIZE 4096
 
-int send_all(int socket, const void *buffer, size_t len)
+int send_all(int fd, const void *buffer, size_t len)
 {
     size_t n;
     const char *p = buffer;
     while (len > 0) {
-        n = send(socket,buffer,len,0);
+        n = send(fd,p,len,0);
         if ( n <= 0 ) {
             return -1;
         }
         p += n;
         len -= n;
+    }
+    return 0;
+}
+
+int serve_500(int fd)
+{
+    char header[500];
+    sprintf(header,"HTTP/1.1 500 Internal Server Error\r\nContent-Length: 0\r\n\r\n");
+    send_all(fd,header,strlen(header));
+    return 0;
+}
+
+int serve_404(int fd)
+{
+    char header[500];
+    struct stat st;
+    FILE *html_file = fopen("404.html","r");
+    if (html_file == NULL) {
+        serve_500(fd);
+        return 1;
+    }
+
+    stat("404.html",&st);
+    sprintf(header, "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\nContent-Length: %ld\r\n\r\n",st.st_size);
+    send_all(fd, header, strlen(header));
+    
+    int bytes_read;
+    char file_data[BUF_SIZE];
+ 
+    while ((bytes_read = fread(file_data, 1, BUF_SIZE, html_file)) > 0) {
+        if ( send_all(fd,file_data,bytes_read) < 0) {
+            perror("send");
+        }    
     }
     return 0;
 }
@@ -77,15 +110,17 @@ int main()
     socklen_t addr_size = sizeof(guest_addr);
     int new_fd;
     int bytes_read;
-    char *status_msg;
     FILE *html_file;
     
     // main loop where we accept new requests and close the new connection after their request was satisfied
     while (1) {
-        status_msg = "200 OK";
         new_fd = accept(sockfd, (struct sockaddr *)&guest_addr, &addr_size);
+        addr_size = sizeof(guest_addr);
+
         if (new_fd < 0){
             perror("accept");
+            freeaddrinfo(res);
+            close(sockfd);
             return 1;
         }
     
@@ -102,39 +137,52 @@ int main()
             continue;
         } else {
             perror("recv");
+            freeaddrinfo(res);
+            close(sockfd);
             return 1;
         }
         char *f = buf + 5;
         *strchr(f, ' ') = 0;
-    
+
+        if(strstr(f,"..")) {
+            printf("..hello\n");
+            serve_404(new_fd);
+            close(new_fd);
+            continue;
+        }
         html_file = fopen(f,"rb"); // read the file they want 
         if(strlen(f) == 0) {
             f = "index.html";
-            html_file = fopen(f,"rb"); 
-            status_msg = "200 OK";
-        }
-        if (html_file == NULL) {
-            f  = "404.html";
-            status_msg = "404 Not Found";
             html_file = fopen(f,"rb");
-
+        }else if (html_file == NULL) {
+            serve_404(new_fd);
+            close(new_fd);
+            continue;
         }
-    
         char header[500];
+
+        if (html_file == NULL) {
+            serve_404(new_fd);
+            close(new_fd);
+            continue;
+        }
+
         struct stat st;
-        stat(f, &st); // get file size and other stats of the file we need 
-        
-        // send header first
+        // get file size and other stats of the file we need
+        stat(f, &st);
         sprintf(header,
-                "HTTP/1.1 %s\r\n"
+                "HTTP/1.1 200 OK\r\n"
                 "Content-Type: text/html\r\n"
                 "Content-Length: %ld\r\n\r\n",
-                status_msg,
                 st.st_size);
 
         char file_data[BUF_SIZE];
+        
+        // Send header first
         if ( send_all(new_fd, header, strlen(header)) < 0) {
             perror("send");
+            freeaddrinfo(res);
+            close(sockfd);
             return 1;
         }
 
@@ -142,6 +190,8 @@ int main()
         while ((bytes_read = fread(file_data, 1, BUF_SIZE, html_file)) > 0) {
             if ( send_all(new_fd,file_data,bytes_read) < 0) {
                 perror("send");
+                freeaddrinfo(res);
+                close(sockfd);
                 return 1;
             }    
         }
